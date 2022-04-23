ac2e73a718a1e1ce052572b2c9e35347b4ade6e9
spv420 <unomilliono@gmail.com>
2022-04-23 16:33:00 -0400

    fuck

--- js/call.js
+++ js/call.js
@@ -19,14 +19,7 @@ function call(addy) {
 function call4arg(addy, r0, r1, r2, r3) {
 	var arg1 = new Int64("0x" + pad_left(r1.toString(16), '0', 8) + pad_left(r0.toString(16), '0', 8));
 	var arg2 = new Int64("0x" + pad_left(r3.toString(16), '0', 8) + pad_left(r2.toString(16), '0', 8));
-	/*
-	var arg2 = new Int64(r3);
-	arg1 = shiftInt64Left(arg1, 32);
-	arg1 = Add(arg1, r0);
-	arg2 = shiftInt64Left(arg2, 32);
-	arg2 = Add(arg2, r2);
-	*/
-		
+
 	arg1d = arg1.asDouble();
 	arg2d = arg2.asDouble();
 
